f04519cdde9a9647ea73c8d52205d486edd04c02
Natalie Obukohwo <natanath20@gmail.com>
2025-12-27 18:59:09 +0100

    Update namecheap-name-servers.tf

--- module-dns/namecheap-name-servers.tf
+++ module-dns/namecheap-name-servers.tf
@@ -1,6 +1,6 @@
-resource "namecheap_domain_records" "my-domain2-com" {
-  domain = var.domain-name
-  mode = "OVERWRITE" // Warning: this will remove all manually set records
+# resource "namecheap_domain_records" "my-domain2-com" {
+#  domain = var.domain-name
+#  mode = "OVERWRITE" 
 
 nameservers = aws_route53_zone.r53_zone.name_servers
 depends_on = [ aws_route53_zone.r53_zone ]
