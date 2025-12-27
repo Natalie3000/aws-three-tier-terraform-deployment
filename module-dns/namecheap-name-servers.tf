6732be83ecf359ab426f9db501e79b8326a5114e
Natalie Obukohwo <natanath20@gmail.com>
2025-12-27 19:52:12 +0100

    Comment out nameservers configuration
    Comment out nameservers and depends_on in namecheap-name-servers.tf

--- module-dns/namecheap-name-servers.tf
+++ module-dns/namecheap-name-servers.tf
@@ -2,6 +2,6 @@
 #  domain = var.domain-name
 #  mode = "OVERWRITE" 
 
-nameservers = aws_route53_zone.r53_zone.name_servers
-depends_on = [ aws_route53_zone.r53_zone ]
-}
+# nameservers = aws_route53_zone.r53_zone.name_servers
+# depends_on = [ aws_route53_zone.r53_zone ]
+# }
